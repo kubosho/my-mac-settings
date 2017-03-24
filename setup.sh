5f68c28c32697dad7ae4fd6b3bd93bad70adb781
Shota Kubota <kubosho@users.noreply.github.com>
2017-03-25 01:00:16 +0900

    feat(setup): add slack

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ brew cask install atom
 brew cask install dropbox
 brew cask install google-chrome
 brew cask install iterm2
+brew cask install slack
 brew cask install visual-studio-code
 
 open /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app/
