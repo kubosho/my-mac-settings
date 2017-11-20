7dc0e30650a003f8307399d2a1e7b1db84821528
kubosho <ta2@o2p.jp>
2017-11-20 23:29:35 +0900

    feat: add Google Cloud SDK

--- setup.sh
+++ setup.sh
@@ -42,6 +42,7 @@ brew cask install atom
 brew cask install docker
 brew cask install dropbox
 brew cask install google-chrome
+brew cask install google-cloud-sdk
 brew cask install google-japanese-ime
 brew cask install google-play-music-desktop-player
 brew cask install iterm2
