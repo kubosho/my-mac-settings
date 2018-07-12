32435c3ca6ac19986c64e5105d279ee90853ec28
Shota Kubota <ta2@o2p.jp>
2018-07-12 16:00:16 +0900

    feat: add some canary or nightly browser packages

--- brew-cask-install.sh
+++ brew-cask-install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+brew tap caskroom/versions
+
+brew cask install google-chrome-canary
+brew cask install firefox-nightly
+brew cask install safari-technology-preview
+
 brew cask install 1password
 brew cask install adobe-creative-cloud
 brew cask install appcleaner
 brew cask install atom
 brew cask install docker
 brew cask install dropbox
-brew cask install google-chrome
 brew cask install google-cloud-sdk
 brew cask install iterm2
 brew cask install karabiner-elements
