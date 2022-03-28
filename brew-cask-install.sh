022e7c603dced29be26db2ae61842d0eb7c6773f
Shota Kubota <ta2@o2p.jp>
2022-03-28 14:03:23 +0900

    feat(brew-cask-install): remove useless applications

--- brew-cask-install.sh
+++ brew-cask-install.sh
@@ -7,9 +7,7 @@ brew cask install firefox-nightly
 brew cask install safari-technology-preview
 
 brew cask install 1password
-brew cask install adobe-creative-cloud
 brew cask install appcleaner
-brew cask install atom
 brew cask install docker
 brew cask install dropbox
 brew cask install google-cloud-sdk
@@ -17,5 +15,3 @@ brew cask install iterm2
 brew cask install karabiner-elements
 brew cask install slack
 brew cask install visual-studio-code
-
-open /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app/
