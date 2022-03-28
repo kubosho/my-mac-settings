3e8f8aa747ffd49edad4e61133ef6e5509b8a6b8
Shota Kubota <ta2@o2p.jp>
2022-03-28 14:37:12 +0900

    feat(brew-cask-install): install a stable version of browser

--- brew-cask-install.sh
+++ brew-cask-install.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-brew tap caskroom/versions
-
-brew cask install google-chrome-canary
-brew cask install firefox-nightly
-brew cask install safari-technology-preview
-
+brew install google-chrome
+brew install google-chrome-canary
+brew install firefox
+brew install firefox-nightly
+brew install safari-technology-preview
 
 brew install 1password
 brew install appcleaner
