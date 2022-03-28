53684e989d4667e8d6e801b8a23b030c9435beb7
Shota Kubota <ta2@o2p.jp>
2022-03-28 14:47:04 +0900

    feat(brew-cask-install): remove unstable browsers

--- brew-cask-install.sh
+++ brew-cask-install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 brew install google-chrome
-brew install google-chrome-canary
 brew install firefox
-brew install firefox-nightly
-brew install safari-technology-preview
 
 brew install 1password
 brew install appcleaner
@@ -15,3 +12,4 @@ brew install iterm2
 brew install karabiner-elements
 brew install slack
 brew install visual-studio-code
+brew install zoom
