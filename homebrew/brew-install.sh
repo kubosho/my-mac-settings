109a1838b1d84f276fb4664b6a0f9aba5b59e37e
Shota Kubota <ta2@o2p.jp>
2022-03-29 13:42:31 +0900

    feat(brew-install): add cask-drivers tap

--- homebrew/brew-install.sh
+++ homebrew/brew-install.sh
@@ -3,5 +3,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew tap homebrew/cask
+brew tap homebrew/cask-drivers
 brew tap homebrew/cask-fonts
 brew tap rcmdnk/rcmdnkpac
