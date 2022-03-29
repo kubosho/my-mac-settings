4eece73fb2d1c2ec19e0c794d9fee5782bf106eb
Shota Kubota <ta2@o2p.jp>
2022-03-29 11:37:00 +0900

    feat(vim): implement directory existence checks

--- vim.sh
+++ vim.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+VUNDLE_DIR=~/.vim/bundle/Vundle.vim
+BACKUP_DIR=~/.vim_backup/
+SWAP_DIR=~/.vim_swap
+
+if [ ! -d $BACKUP_DIR ]; then
+        mkdir $BACKUP_DIR
+fi
+
+if [ ! -d $SWAP_DIR ]; then
+        mkdir $SWAP_DIR
+fi
+
+if [ ! -d $VUNDLE_DIR ]; then
+        git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
+fi
