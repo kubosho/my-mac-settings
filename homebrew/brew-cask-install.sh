3990adc28589d44c9f959d16dcb7ff7d612ee07d
KUBOTA Shota <ta2@o2p.jp>
2025-02-17 18:56:52 +0900

    feat(brew-cask-install): remove unnecessary softwares

--- homebrew/brew-cask-install.sh
+++ homebrew/brew-cask-install.sh
@@ -5,20 +5,15 @@ brew install --cask firefox
 
 brew install --cask 1password
 brew install --cask alt-tab
-brew install --cask appcleaner
 brew install --cask bettertouchtool
 brew install --cask codeql
 brew install --cask dropbox
-brew install --cask elgato-stream-deck
 brew install --cask figma
-brew install --cask font-fira-code
 brew install --cask forticlient
 brew install --cask google-cloud-sdk
 brew install --cask imageoptim
 brew install --cask karabiner-elements
-brew install --cask logitech-g-hub
 brew install --cask obsidian
 brew install --cask slack
 brew install --cask visual-studio-code
-brew install --cask warp
 brew install --cask zoom
