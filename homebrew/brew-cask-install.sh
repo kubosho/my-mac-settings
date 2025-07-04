d85db74d8897bd6d4d12a53dfb21c587b2054d6d
KUBOTA Shota <ta2@o2p.jp>
2025-07-04 18:14:26 +0900

    feat(homebrew): add missing cask packages
    Add the following cask packages to the installation script:
    - bitwarden: password manager
    - coteditor: text editor for macOS
    - gather: virtual office platform
    - ticktick: to-do list app
    - wezterm: terminal emulator

--- homebrew/brew-cask-install.sh
+++ homebrew/brew-cask-install.sh
@@ -6,12 +6,17 @@ brew install --cask firefox
 brew install --cask 1password
 brew install --cask alt-tab
 brew install --cask bettertouchtool
+brew install --cask bitwarden
 brew install --cask claude
+brew install --cask coteditor
 brew install --cask cursor
 brew install --cask dropbox
 brew install --cask figma
+brew install --cask gather
 brew install --cask obsidian
 brew install --cask proxyman
 brew install --cask slack
+brew install --cask ticktick
 brew install --cask visual-studio-code
+brew install --cask wezterm
 brew install --cask zoom
