5ba26fd9cfb774d5128fdc51bed07a387d61b9a7
KUBOTA Shota <ta2@o2p.jp>
2025-07-04 16:47:56 +0900

    chore(homebrew): remove unused softwares

--- homebrew/brew-cask-install.sh
+++ homebrew/brew-cask-install.sh
@@ -7,14 +7,9 @@ brew install --cask 1password
 brew install --cask alt-tab
 brew install --cask bettertouchtool
 brew install --cask claude
-brew install --cask codeql
 brew install --cask cursor
 brew install --cask dropbox
 brew install --cask figma
-brew install --cask forticlient
-brew install --cask google-cloud-sdk
-brew install --cask imageoptim
-brew install --cask karabiner-elements
 brew install --cask obsidian
 brew install --cask proxyman
 brew install --cask slack
