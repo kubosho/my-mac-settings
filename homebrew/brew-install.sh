cc4096390eb2124bbfa85a3a99e4a295ab20b044
KUBOTA Shota <ta2@o2p.jp>
2025-07-04 19:02:11 +0900

    fix(homebrew): add PATH setup for Apple Silicon Mac
    Add eval command to set up Homebrew PATH immediately after installation
    to prevent "brew command not found" error on new Mac setup

--- homebrew/brew-install.sh
+++ homebrew/brew-install.sh
@@ -2,6 +2,9 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Set up Homebrew PATH for Apple Silicon Mac
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew tap homebrew/cask
 brew tap homebrew/cask-fonts
 brew tap rcmdnk/rcmdnkpac
