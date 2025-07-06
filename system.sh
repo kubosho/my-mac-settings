3af1ef6c9af829e33dbced519cc4d53c2eb6c473
kubosho <ta2+github@o2p.jp>
2025-07-06 13:07:40 +0900

    feat(system): update macOS settings

--- system.sh
+++ system.sh
@@ -16,3 +16,12 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 # KeyRepeat speed
 defaults write -g InitialKeyRepeat -int 12
 defaults write -g KeyRepeat -int 2
+
+# Set cursor tracking speed to maximum (beyond UI limit)
+defaults write -g com.apple.trackpad.scaling -float 10.0
+
+# Disable display resolution scaling (more space setting)
+defaults write NSGlobalDomain AppleDisplayScaleFactor -float 0.75
+
+# Disable live conversion for Japanese IME
+defaults write com.apple.inputmethod.Kotoeri JIMPrefLiveConversionKey -bool false
